3d9ab103510b75cf20e725e135c234fbe562184c
Kliucherova Irina <klucerovaira@gmail.com>
2025-09-03 21:37:11 +0200

    commit

--- lib/api.ts
+++ lib/api.ts
@@ -1,28 +1,29 @@
 import axios from "axios";
 import type { Note } from "@/types/note";
 
-const NOTEHUB_KEY = process.env.NEXT_PUBLIC_NOTEHUB_KEY;
-const LINK = 'https://notehub-public.goit.study/api/notes';
+const NOTEHUB_TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+const LINK = "https://notehub-public.goit.study/api/notes";
 
 interface NoteResponse {
     notes: Note[];
     totalPages: number;
 }
 
-export async function fetchNotes(page: number, userQuery: string = ""): Promise<NoteResponse> {
-
-    const response = await axios.get<NoteResponse>(LINK,
-        {
-            params: {
-                search: userQuery,
-                page: page,
-                perPage: 12,
-            },
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${NOTEHUB_KEY}`,
-            }
-        });
+export async function fetchNotes(
+    page: number,
+    userQuery: string = ""
+): Promise<NoteResponse> {
+    const response = await axios.get<NoteResponse>(LINK, {
+        params: {
+            search: userQuery,
+            page: page,
+            perPage: 12,
+        },
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${NOTEHUB_TOKEN}`,
+        },
+    });
 
     return response.data;
 }
@@ -30,9 +31,9 @@ export async function fetchNotes(page: number, userQuery: string = ""): Promise<
 export async function fetchNoteById(id: string): Promise<Note> {
     const response = await axios.get<Note>(`${LINK}/${id}`, {
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${NOTEHUB_KEY}`
-        }
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${NOTEHUB_TOKEN}`,
+        },
     });
     return response.data;
 }
@@ -40,15 +41,15 @@ export async function fetchNoteById(id: string): Promise<Note> {
 export interface CreateNoteParams {
     title: string;
     content: string;
-    tag: 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
+    tag: "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
 }
 
 export async function createNote(newNote: CreateNoteParams): Promise<Note> {
     const response = await axios.post<Note>(LINK, newNote, {
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${NOTEHUB_KEY}`,
-        }
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${NOTEHUB_TOKEN}`,
+        },
     });
     return response.data;
 }
@@ -56,9 +57,9 @@ export async function createNote(newNote: CreateNoteParams): Promise<Note> {
 export async function deleteNote(id: string): Promise<Note> {
     const response = await axios.delete<Note>(`${LINK}/${id}`, {
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${NOTEHUB_KEY}`,
-        }
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${NOTEHUB_TOKEN}`,
+        },
     });
     return response.data;
 }
