b2f0f663f126452ae2167ce571bccf99d5ab1fd6
Kliucherova Irina <klucerovaira@gmail.com>
2025-09-03 20:53:00 +0200

    commit

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+// global.d.ts
+
+declare module "*.css";
+declare module "*.scss";
+declare module "*.sass";
+
+declare module "*.module.css";
+declare module "*.module.scss";
+declare module "*.module.sass";
